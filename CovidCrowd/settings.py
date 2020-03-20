1092b33692c25ccb4fe90c36396fb5da68936ec4
Arunmozhi <arun@arunmozhi.in>
2020-03-20 23:00:14 +0530

    adds allowed hosts loding from env

--- CovidCrowd/settings.py
+++ CovidCrowd/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config("MY_HOST")]
 
 
 # Application definition
