d4431a24ea020f9f1d8db66d2d3c960fc0f2e3ed
Arunmozhi <arun@arunmozhi.in>
2020-03-22 10:29:56 +0530

    fixed the template name for patients table

--- patients/tables.py
+++ patients/tables.py
@@ -36,7 +36,7 @@ class PatientsTable(tables.Table):
 
     class Meta:
         model = Patient
-        template_name = "django_tables2/bootstrap.html"
+        template_name = "django_tables2/bootstrap4.html"
         fields = (
             "id",
             "diagnosed_date",
