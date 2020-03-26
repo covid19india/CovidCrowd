2e6372fbc3d01c70dab0b8749758ae05332fe2ff
Arunmozhi <arun@arunmozhi.in>
2020-03-26 13:13:38 +0530

    renamed the django static url

--- CovidCrowd/settings.py
+++ CovidCrowd/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/djstatic/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Crispy forms
